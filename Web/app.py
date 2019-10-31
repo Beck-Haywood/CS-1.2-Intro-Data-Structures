5361ebb4940471cf0bc0b694a0a9f33dc4b65177
Beck-Haywood <beck.haywood@students.makeschool.com>
2019-10-31 14:02:31 -0700

    added Procfile

---
+++ Web/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from histogram import *
+from sample_word import random_sentence
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def sentence():
+    word_list = read_word_file('words.txt')
+    histogram = make_histogram(word_list)
+    return random_sentence(histogram, 10)
+
+if __name__ == '__main__':
+    app.run(debug=True)
